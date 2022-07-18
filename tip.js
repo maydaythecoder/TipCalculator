6756818b6b96fa26aa23864110bc1ac049460aea
janeanekim <jmk90020@gmail.com>
2022-07-18 07:20:42 -0700

    js and css components

--- tip.js
+++ tip.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+let billAmountElement = document.querySelector("#BillAmount");
+let numGuestsElement = document.querySelector("#NumberOfGuests");
+let serviceQualElement = document.getElementById("service");
+let div = document.getElementById('Total');
+let button = document.querySelector('button');
+
+function total(){
+    let billAmount = Number(billAmountElement.value);
+    let numGuests = Number(numGuestsElement.value);
+    let serviceQual = Number(serviceQualElement.value);
+    let billTotal = ((billAmount * serviceQual)+ billAmount)/numGuests;
+    console.log(billTotal);
+
+    div.innerText= billTotal.toFixed(2);
+} 
+
+button.addEventListener('click', total);
