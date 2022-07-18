b3746eb02549bd08fe6d7a34d75d712f0f5b6058
mmohamed <maydaythecreator@gmail.com>
2022-07-18 10:17:49 -0400

    Finished up tip calculator

--- tip.js
+++ tip.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+
+let billAmountElement = document.querySelector("#billAmount");
+let numGuestsElement = document.querySelector("#numberOfGuests");
+let serviceQualElement = document.getElementById("service");
+let div = document.getElementById('Total');
+let button = document.querySelector('button');
+
+function total(){
+    let billAmount = Number(billAmountElement.value);
+    let numGuests = Number(numGuestsElement.value);
+    let serviceQual = Number(serviceQualElement.value);
+    let billTotal = ((billAmount * serviceQual)+ billAmount)/numGuests;
+    console.log(billTotal);
+
+    div.innerText= billTotal.toFixed(2);
+}
+
+button.addEventListener('click', total);
